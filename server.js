8f46f8b759cce21cfff83cf40d6c6dd1aeba1282
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-19 14:31:37 -0700

    vevev

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static("public"))
+app.set("veiw engine", "ejs");
 
-app.get("/dog", function(req, res){
- res.send("hi tere");
+app.get("/", function(req, res){
+ res.render("home");
 });
 
 app.get('*' , function(req, res ){
- res.send("you are a star"); 
+ res.send("you are a star");
 });
 
-app.listen(process.env.PORT ,process.env.ip || 8080, () => console.log("all is ok"));
+app.listen(process.env.PORT ,process.env.ip || 8080, () => console.log("yelpcamp server has sarted"));
