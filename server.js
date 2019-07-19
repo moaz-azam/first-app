a1f7e8ed8d0704ab7b9e6bafce71ffa30d4e2dea
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-19 15:05:01 -0700

    vevev

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ app.get("/", function(req, res){
  res.render("home");
 });
 
-app.get('*' , function(req, res ){
- res.send("you are a star");
+app.get("/campgrounds", function(req , res){
+  var campgrounds = [
+   {name: "hunza", image:"https://pixabay.com/get/57e8dc414e5ba814f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+   {name: "murreee", image:"https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+   {name: "sakardu", image:"https://pixabay.com/get/54e8d7464b5bab14f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+   {name: "swat", image:"https://pixabay.com/get/54e8d7464b5bab14f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+ ];
+ res.render("campgrounds");
 });
 
-app.listen(process.env.PORT ,process.env.ip || 8080, () => console.log("yelpcamp server has sarted"));
+//app.get('*' , function(req, res ){
+ //res.send("you are a star");
+//});
+
+app.listen(process.env.PORT ,process.env.IP || 8080, () => console.log("yelpcamp server has sarted"));
