f46960b064753a3656891a9cf3cbe0639ce596da
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-19 15:58:46 -0700

    vevev

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static("public"));
-app.set("veiw engine", "ejs");
+app.set("view engine", "ejs");
 
 
 app.get("/", function(req , res){
