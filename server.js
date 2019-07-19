c1a95a827208405491c3e91cb1f87ee2c70e046b
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-18 21:31:22 -0700

    sd

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.get("/dog", function(req, res){
  res.send("hi tere");
 });
 
+app.get('*' , function(req, res ){
+ res.send("you are a star"); 
+});
 
 app.listen(process.env.PORT ,process.env.ip || 8080, () => console.log("all is ok"));
