46cc780ddd58f467077daca285f45f89132c7a25
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-18 18:53:35 -0700

    v2

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
+var express = require('express');
+var app = express();
 
-const app = express();
+app.use(express.static("public"))
 
-app.use(express.static("public"));
+app.get("/", function(req, res){
+ res.send("hi tere");
+});
+
 
 app.listen(process.env.PORT || 8080, () => console.log("all is ok"))
