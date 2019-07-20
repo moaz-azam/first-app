4c36087a971a238c1b580aa302de37bf103e835d
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-20 02:00:45 -0700

    xd

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ var campgrounds = [
   {name: "murreee", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
   {name: "sakardu", image: "https://pixabay.com/get/54e8d7464b5bab14f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
   {name: "swat", image: "https://pixabay.com/get/54e8d7464b5bab14f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+  {name: "hunza", image: "https://pixabay.com/get/57e8dc414e5ba814f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+  {name: "murreee", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+  {name: "sakardu", image: "https://pixabay.com/get/54e8d7464b5bab14f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
+  {name: "swat", image: "https://pixabay.com/get/54e8d7464b5bab14f6da8c7dda793f7f1636dfe2564c704c732c7fdd9048c45d_340.jpg"},
 ];
 
 app.get("/", function(req , res){
