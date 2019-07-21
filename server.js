802647f43d11452b0e2fc209cac1f43876b0529f
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-21 16:23:21 -0700

    v2

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost/yelp-camp")
 
 app.use(bodyparser.urlencoded({extended: true}));
 
 //app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+var campgroundSchema = new mongoose.Schema({
+ name: "string",
+ image: "string",
+});
+
+var campground = mongoose.model("campground",campgroundSchema);
+campground.create(
+ {
+  name: "hunza",
+  image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"
+ }, function(err,campground){
+  if(err){
+  console.log(err);
+ }else{
+  console.log("newly create campground");
+  console.log(campground);
+ }
+});
+
 var campgrounds = [
-  {name: "hunza", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "murreee", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "sakardu", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "swat", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "hunza", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "murreee", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "sakardu", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
-  {name: "swat", image: "https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732c7dd4914fcd5e_340.jpg"},
+
 ];
 
 app.get("/", function(req , res){
@@ -23,7 +38,13 @@ app.get("/", function(req , res){
 });
 
 app.get("/campgrounds", function(req , res){
- res.render("campgrounds", {campgrounds:campgrounds});
+ campground.find({}, function (err, campgrounds){
+  if(err){
+   console.log(err);
+  }else {
+    res.render("campgrounds", {campgrounds:campgrounds});
+  }
+ });
 });
 
 app.post("/campgrounds", function(req, res){
