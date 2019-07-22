4d0dd5b5abf6da9187524130e1d7aeb3c88e590d
Moaz Azam <moazazam4356@gmail.com.com>
2019-07-21 17:20:50 -0700

    l

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.get("/campgrounds/new", function(req, res){
  res.render("new.ejs");
 });
 
-app.listen(process.env.PORT ,process.env.IP || 8080, () => console.log("yelpcamp server has sarted"));
+//app.listen(process.env.PORT ,process.env.IP || 8080, () => console.log("yelpcamp server has sarted"));
 socket = io.listen(process.env.PORT);
